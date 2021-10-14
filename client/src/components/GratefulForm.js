efb4cf1e120aaebf1b8b5eb07c429e7ac5af6721
Janay Anguiano <janayanguiano@gmail.com>
2021-10-14 14:41:47 -0700

    grateful input

--- client/src/components/GratefulForm.js
+++ client/src/components/GratefulForm.js
@@ -4,7 +4,14 @@ const GratefulForm = () => {
   return (
     <div className="grateful-form">
       <form>
-        <input></input>
+        <p>
+          {' '}
+          Take this time to tell us what you are grateful for, there is nothing
+          too small. In retrospect we begin to see how all the little things
+          that we are grateful for add up into creating our 'why's' in life.
+        </p>
+        <h1>What are you Grateful for? </h1>
+        <input className="grateful-input"></input>
       </form>
     </div>
   )
