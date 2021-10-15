2a9ae4e91aa7db0810b9a65eec5aaffa3c4c6e03
Janay Anguiano <janayanguiano@gmail.com>
2021-10-14 18:04:01 -0700

    journal form

--- client/src/components/JournaliesForm.js
+++ client/src/components/JournaliesForm.js
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { Form } from 'react-bootstrap'
 const JournaliesForm = () => {
-  return <div className="journal-container"></div>
+  return (
+    <div className="journal-container">
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Journal Title</Form.Label>
+          <Form.Control type="text" placeholder="Journal Title" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Entry </Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+    </div>
+  )
 }
 
 export default JournaliesForm
