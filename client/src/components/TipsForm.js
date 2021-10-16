af263ca50ffdf834817726cad6e603b45228c1bb
Janay Anguiano <janayanguiano@gmail.com>
2021-10-16 12:26:09 -0700

    adding all crud for tips

--- client/src/components/TipsForm.js
+++ client/src/components/TipsForm.js
@@ -6,6 +6,24 @@ import { BASE_URL } from '../globals'
 const TipsForm = (props) => {
   const [tip, setTip] = React.useState(null)
 
+const getTip = () => {
+  axios.get(BASE_URL).then((response) => {
+    setTip(response.data)
+  })
+}
+if (!tip) return null
+
+
+const updateTip = () => {
+  axios.put(`${BASE_URL}/1`, {
+    content: req.body.content
+  })
+    .then((response) => {
+      setTip(response.data)
+    })
+}
+if(!tip) return "No Post!"
+
   const createTip = () => {
     axios
       .post(BASE_URL, {
@@ -24,6 +42,11 @@ const TipsForm = (props) => {
     })
   }
   if (!tip) return 'No Tip!'
+
+const 
+
+
+
   return (
     <div className="TipsForm">
       <h2>{tip} </h2>
@@ -35,9 +58,13 @@ const TipsForm = (props) => {
         <button type="submit" onClick={createTip}>
           Add
         </button>
-        <button tyoe="delete" onClick={deleteTip}>
+        <button type="delete" onClick={deleteTip}>
           Delete
         </button>
+        <button type="edit" onClick={updateTip}>
+          Edit
+        </button>
+        <input type="showTip" getTip={getTip}>Get Tip</input>
       </form>
     </div>
   )
